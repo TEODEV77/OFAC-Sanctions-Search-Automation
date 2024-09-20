0609b36deaebaadac0d716b9eb481e8c349569a6
Mateo <mateo.orolot@gmail.com>
2024-09-20 15:50:05 -0500

    Add execute_query function with support for select, insert, and delete operations, including error handling and rollback

--- src/postgre_sql_config.py
+++ src/postgre_sql_config.py
@@ -23,3 +23,40 @@ def connect_to_db():
     except Exception as e:
         print(f"Unexpected error: {e}")
         return None
+    
+def execute_query(query, params=(),action=None):
+        connection = connect_to_db() 
+        try:
+            with connection.cursor() as cursor:
+                if action == "select":
+                    cursor.execute(query, params)
+                    return cursor.fetchall()
+                
+                elif action == "insert":
+                    cursor.execute(query, params)
+                    connection.commit()
+                
+                elif action == "delete":
+                    cursor.execute(query, params)
+                    connection.commit()
+                
+                print("Query executed successfully.")
+                
+        except DatabaseError as db_error:
+            print(f"Database error: {db_error}")
+            connection.rollback()
+            return None
+        
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            connection.rollback()
+            return None
+        
+def select(query, params=()):
+    return execute_query(query,params,action="select")
+
+def insert(query, params=()):
+    execute_query(query,params,action="insert")
+    
+def delete(query, params=()):
+    execute_query(query,params,action="delete")
